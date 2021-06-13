a05d9e4fedf9c8b25710d3acf44fd3a899a7b816
Nicole de Guzman <nicoledeguzman@Nicoles-MacBook-Air-2.local>
2021-06-13 01:15:51 -0400

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo-doctalk.png';
+
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Link,
+  Switch 
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <img src={logo} alt="doctalk logo"/>
+          <nav>
+            <button className="menu">nav</button>
+            <ul>
+              <li><p>Physicians</p>
+                <ul class="sub-menu">
+                  <li><Link to="/physician-apps">Applications</Link></li>
+                  <li><Link to="/app-ideas">Application Ideas</Link></li>
+                  <li><Link to="/support">Support</Link></li>
+                </ul>
+              </li>
+              <li><p>Organizations</p>
+                <ul class="sub-menu">
+                  <li><Link to="/organization-apps">Applications</Link></li>
+                  <li><Link to="/app-ideas">Application Ideas</Link></li>
+                  <li><Link to="/support">Support</Link></li>
+                </ul>
+              </li>
+              <li><p>Medical Trainees</p>
+                <ul class="sub-menu">
+                  <li><Link to="/med-trainee-apps">Applications</Link></li>
+                </ul>
+              </li>
+              <li><p>Company</p>
+                <ul class="sub-menu">
+                  <li><Link to="/about">About Us</Link></li>
+                  <li><Link to="/news">News</Link></li>
+                  <li><Link to="/careers">Careers</Link></li>
+                  <li><Link to="/partners">Partners</Link></li>
+                  <li><Link to="/contact">Contact Us</Link></li>
+                </ul>
+              </li>
+            </ul>
+          </nav>
+          <a href="">Log In</a>
+        </header>
+      </div>
+    </ Router>
   );
 }
 
